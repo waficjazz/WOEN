6eff150a52b6adba5e0c24e5ac0c83648d91a01f
wafic <waficojazzaro@gmail.com>
2022-09-18 09:19:19 +0300

    add graph parse functions

--- WASM/src/lib.rs
+++ WASM/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 
 #[wasm_bindgen]
-pub fn read_file(a: String) -> String {
+pub fn getOneWay(a: String) -> String {
     let mut _map: HashMap<&str , Vec<&str>> = HashMap::new();
     a.lines().for_each(|line| {
         let vec: Vec<&str> = line.split(">").collect();
@@ -18,6 +18,43 @@ pub fn read_file(a: String) -> String {
         }
     });
 
+    let mut result = String::new();
+    result.push_str("{");
+    let mut i:u32 = 0;
+    for (key, value) in _map.iter() {
+        i = i+1;
+        if i < _map.len().try_into().unwrap() {
+            result.push_str(&format!("\"{}\":{:?},", key, value));
+        }else {
+            result.push_str(&format!("\"{}\":{:?}", key, value));
+        }
+    }
+    result.push_str("}");
+    result
+}
+
+#[wasm_bindgen]
+pub fn getTwoWay(a: String) -> String {
+    let mut _map: HashMap<&str , Vec<&str>> = HashMap::new();
+    a.lines().for_each(|line| {
+        let vec: Vec<&str> = line.split(">").collect();
+        let key = vec[0].trim();
+        let value = vec[1].trim();
+        if _map.contains_key(key) {
+            let mut vec = _map.get(key).unwrap().to_vec();
+            vec.push("n_"+value);
+            _map.insert(key, vec);
+        } else {
+            _map.insert(key, vec!["n_"+value]);
+        }
+        if _map.contains_key(value) {
+            let mut vec = _map.get(value).unwrap().to_vec();
+            vec.push("p_"+key);
+            _map.insert(value, vec);
+        } else {
+            _map.insert(value, vec!["p_"+key]);
+        }
+    });
     let mut result = String::new();
     result.push_str("{");
     let mut i:u32 = 0;
