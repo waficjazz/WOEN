47b75dad33ebd089de2178b14e3dc83cc870a507
wafic <waficojazzaro@gmail.com>
2022-09-18 14:07:27 +0300

    fix wasm

--- WASM/src/lib.rs
+++ WASM/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 
 
 #[wasm_bindgen]
-pub fn getOneWay(a: String) -> String {
+pub fn get_one_way(a: String) -> String {
     let mut _map: HashMap<&str , Vec<&str>> = HashMap::new();
     a.lines().for_each(|line| {
         let vec: Vec<&str> = line.split(">").collect();
@@ -34,25 +34,31 @@ pub fn getOneWay(a: String) -> String {
 }
 
 #[wasm_bindgen]
-pub fn getTwoWay(a: String) -> String {
-    let mut _map: HashMap<&str , Vec<&str>> = HashMap::new();
+pub fn get_two_way(a: String) -> String {
+    let mut _map: HashMap<String , Vec<String>> = HashMap::new();
     a.lines().for_each(|line| {
         let vec: Vec<&str> = line.split(">").collect();
-        let key = vec[0].trim();
-        let value = vec[1].trim();
-        if _map.contains_key(key) {
-            let mut vec = _map.get(key).unwrap().to_vec();
-            vec.push("n_"+value);
+        let key =vec[0].trim().to_string();
+        let value = vec[1].trim().to_string();
+        let key1 =vec[0].trim().to_string();
+        let value1 = vec[1].trim().to_string();
+        if _map.contains_key(&key) {
+            let mut vec = _map.get(&key).unwrap().to_vec();
+            let s = "n_".to_owned()+ &value;
+            vec.push(s);
             _map.insert(key, vec);
         } else {
-            _map.insert(key, vec!["n_"+value]);
+            let s = "n_".to_owned()+ &value;
+            _map.insert(key, vec![s]);
         }
-        if _map.contains_key(value) {
-            let mut vec = _map.get(value).unwrap().to_vec();
-            vec.push("p_"+key);
-            _map.insert(value, vec);
+        if _map.contains_key(&value1) {
+            let mut vec = _map.get(&value1).unwrap().to_vec();
+            let s = "p_".to_owned()+ &key1;
+            vec.push(s);
+            _map.insert(value1, vec);
         } else {
-            _map.insert(value, vec!["p_"+key]);
+            let s = "p_".to_owned()+ &key1;
+            _map.insert(value1, vec![s]);
         }
     });
     let mut result = String::new();
--- front/src/graph/graph.tsx
+++ front/src/graph/graph.tsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
-import init, { read_file } from "wasm-lib";
+import React, { useState, useEffect } from "react";
+import init from "wasm-lib";
 
 const graph = () => {
   const [data, setData] = useState();
   const [twoWatData, setTwoWayData] = useState();
   const [dataLevels, setDataLevels] = useState();
-  return <div></div>;
+  const [file, setFile] = useState<String>("");
+
+  function readTextFile(file: string) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
+          var allText = rawFile.responseText;
+          setFile(allText);
+          return allText.toString();
+        }
+      }
+    };
+    rawFile.send(null);
+  }
+
+  return <div className=""></div>;
 };
 
 export default graph;
