4d7eda205f9f660107001113fa052b308d379c27
wafic <waficojazzaro@gmail.com>
2022-09-18 12:01:28 +0300

    update main structure

---
+++ front/src/graph/graph.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import init, { read_file } from "wasm-lib";
+
+const graph = () => {
+  const [data, setData] = useState();
+  const [twoWatData, setTwoWayData] = useState();
+  const [dataLevels, setDataLevels] = useState();
+  return <div></div>;
+};
+
+export default graph;
