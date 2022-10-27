4194b4a9020ad8d16435b134bf3ff8046ad55290
Wafic Jazzar <whjazzar@bluemint.ai>
2022-10-27 21:42:13 +0300

    create workflow

---
+++ front/src/Dashboard/Workflows/CWorkflow/CFrom.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CWorkflow.css";
+
+const CFrom = () => {
+  return <div className="workflow_form">CFrom</div>;
+};
+
+export default CFrom;
--- front/src/Dashboard/Workflows/Workflows.tsx
+++ front/src/Dashboard/Workflows/Workflows.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import Button from "../../shared/Buttons/Button";
+import CFrom from "./CWorkflow/CFrom";
 import "./Workflows.css";
 
 const Workflows = () => {
+  const [showForm, setShowForm] = useState(false);
   return (
-    <div className="workflow_board">
-      <div className="container_board_header">
-        <p>Workflows</p>
-        <Button>Create</Button>
+    <>
+      <div className="workflow_board">
+        <div className="container_board_header">
+          <p>Workflows</p>
+          <Button onClick={() => setShowForm(true)}>Create</Button>
+        </div>
       </div>
-    </div>
+      {showForm && <CFrom />}
+    </>
   );
 };
 
