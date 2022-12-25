61294dbed6a42b4d1682059789e9a367797f5a6d
waficjazz <waficojazzaro@gmail.com>
2022-12-25 13:27:43 +0200

    added job summary in detail name start end duration status

--- front/src/Dashboard/Workflows/JobDetails.tsx
+++ front/src/Dashboard/Workflows/JobDetails.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { IWJob } from "../../types";
+import { getDuration, dateStyle } from "../../utils/time-format";
+import ReactTimeAgo from "react-time-ago";
 
 interface IProps extends IWJob {}
 const JobDetails = ({ ...props }: IProps) => {
@@ -7,16 +9,41 @@ const JobDetails = ({ ...props }: IProps) => {
 
   const selectedStyle = {
     borderBottom: "1px solid white ",
-    transition: "border 1s ease-in-out",
   };
   return (
     <div className="job_details_container">
       <div className="job_details_options">
-        <div style={option === 1 ? selectedStyle : {}} onClick={() => setOption(1)}>
+        <div style={option === 1 ? selectedStyle : {}} className="tab" onClick={() => setOption(1)}>
           SUMMARY
         </div>
         <div style={option === 2 ? selectedStyle : {}} onClick={() => setOption(2)}>
           CONTAINER
+        </div>
+      </div>
+      <div className="job_details_summary">
+        <div>
+          <label>NAME</label>
+          <p>{props.name}</p>
+        </div>
+        <div>
+          <label>STATUS</label>
+          <p>{props.status}</p>
+        </div>
+        <div>
+          <label>START TIME</label>
+          <p>{(props.startedAt && <ReactTimeAgo date={new Date(props.startedAt)} locale="en-US" timeStyle={dateStyle} />) || "-"}</p>
+        </div>
+        <div>
+          <label>END TIME</label>
+          <p>{(props.finishedAt && <ReactTimeAgo date={new Date(props.finishedAt)} locale="en-US" timeStyle={dateStyle} />) || "-"}</p>
+        </div>
+        <div>
+          <label>DURATION</label>
+          <p>
+            {(props.finishedAt && props.startedAt && getDuration(new Date(props.startedAt), new Date(props.finishedAt))) ||
+              (props.startedAt && <ReactTimeAgo date={new Date(props.startedAt)} locale="en-US" timeStyle="mini" />) ||
+              "-"}
+          </p>
         </div>
       </div>
     </div>
