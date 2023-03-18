0317513eb556deb6f360d30e5c7af49391194c7d
waficjazz <waficojazzaro@gmail.com>
2023-03-18 20:22:29 +0200

    add coords funciton

--- back/src/controllers/w-template-controller.ts
+++ back/src/controllers/w-template-controller.ts
@@ -3,24 +3,29 @@ import { IFJOB, IJob, RequestWithUserId } from "../types";
 import { updateJob, updateJobTemplate } from "../services/job-services";
 import { jobTemplate, PrismaClient, workflowTemplate, container } from "@prisma/client";
 const HttpError = require("../utils/http-error");
-import { createJobTemplate, initTemplate } from "../services/w-template-services";
+import { createJobTemplate, initTemplate, getCoords } from "../services/w-template-services";
 
 const submitWorkflowTemplate = async (req: RequestWithUserId, res: Response, next: NextFunction) => {
   const { name, projectId, params, jobs } = req.body;
   let template: workflowTemplate;
+  let coords = {};
   try {
-    template = await initTemplate(name, req.userId, projectId, params);
+    // template = await initTemplate(name, req.userId, projectId, params);
+    coords = getCoords(jobs);
+    console.log("coors", coords);
     await Promise.all(
       jobs.map(async (job: IFJOB) => {
         await createJobTemplate(job, template.id);
       })
     );
+    //update template with placements
+    //update jobs with dependencies and successors
   } catch (err) {
     const error = new HttpError("Could not create workflow template.", 500);
     console.log(err);
     return next(error);
   }
-  res.status(201).json(template);
+  res.status(201).json({});
 };
 
 const createWorkflowTemplate = async (req: RequestWithUserId, res: Response, next: NextFunction) => {
--- back/src/services/w-template-services.ts
+++ back/src/services/w-template-services.ts
@@ -7,6 +7,17 @@ import { checkCondtion, updateJob } from "./job-services";
 import { pushParamsArgs } from "@redis/search/dist/commands";
 const prisma = new PrismaClient();
 
+let drawed: string[] = [];
+interface succers {
+  [key: string]: string[];
+}
+
+type Placement = [number, number];
+let levels: Levels = {};
+interface Levels {
+  [key: number]: string[];
+}
+
 export const initTemplate = async (name: string, uid: number, projectId: number, params: ITParams[]) => {
   let workflow: workflowTemplate;
   try {
@@ -50,6 +61,90 @@ export const createJobTemplate = async (j: IFJOB, wtid: number) => {
     throw err;
   }
   return container;
+};
+
+function getRoot(jobArray: IJob[]) {
+  console.log(jobArray);
+  let arr: string[] = [];
+  jobArray.map((job) => {
+    if (job.dependencies && job.dependencies.length == 0) {
+      arr.push(job.name);
+    }
+  });
+  console.log(arr);
+  return arr;
+}
+
+function getDependencyPlacement(jobArray: IJob[]) {
+  let succers: succers = {};
+  jobArray.map((job) => {
+    if (job.dependencies) {
+      job.dependencies.map((dep) => {
+        if (succers[dep]) succers[dep] = [...succers[dep], job.name];
+        else succers[dep] = [job.name];
+      });
+    }
+  });
+  let root = getRoot(jobArray);
+  let tmp: string[] = [];
+  let i = 0;
+  levels[0] = root;
+  let nextLevel: string[] = [];
+  nextLevel = nextLevel.concat(root);
+  while (nextLevel.length > 0) {
+    tmp = [];
+    i++;
+    nextLevel.forEach((element) => {
+      if (succers === undefined) return 0;
+      if (succers[element] !== undefined) {
+        tmp = tmp.concat(succers[element]);
+      }
+    });
+    levels[i] = [...new Set(tmp)];
+    nextLevel = tmp;
+  }
+  Object.entries(levels).forEach(([key, value]) => {
+    value.forEach((e: string) => {
+      if (!drawed.includes(e)) {
+        drawed.push(e);
+      } else {
+        Object.entries(levels).forEach(([key1, value1]) => {
+          value1.forEach((e1: string) => {
+            if (key !== key1) {
+              if (e1 === e) {
+                value1.splice(value1.indexOf(e1), 1);
+              }
+            }
+          });
+        });
+      }
+    });
+  });
+  return levels;
+}
+
+interface coor {
+  [key: string]: [number, number];
+}
+let coors: coor = {};
+
+export const getCoords = (jobArray: IJob[]) => {
+  console.log("see coors", jobArray);
+  let levels = getDependencyPlacement(jobArray);
+  console.log("see levels", levels);
+  Object.entries(levels).forEach(([key, value]) => {
+    let x = 0;
+    value.map((val: string, i: number) => {
+      if (val && key) {
+        coors[val] = [x, Number(key)];
+        if (i % 2 == 0) {
+          x++;
+        }
+        x = x * -1;
+      }
+    });
+  });
+  return coors;
 };
 
 // userId: req.userId,
