2af04372fe451f884e51a6971c2b1c557af2b250
waficjazz <waficojazzaro@gmail.com>
2023-03-24 17:52:06 +0200

    update saved container row style

--- front/src/Dashboard/ContainerBoard/SavedContainerRow.tsx
+++ front/src/Dashboard/ContainerBoard/SavedContainerRow.tsx
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBox, faPlay, faTrashCan, faFloppyDisk, faPause } from "@fortawesome/free-solid-svg-icons";
 import { ISContainer } from "../../types";
+import ReactTimeAgo from "react-time-ago";
+import { dateStyle } from "../../utils/time-format";
+import "./styles.css";
 interface Props extends ISContainer {}
 
 const SavedContainerRow = (props: Props) => {
@@ -16,13 +19,17 @@ const SavedContainerRow = (props: Props) => {
 
   return (
     <>
-      <div className="container_row">
-        <FontAwesomeIcon icon={faBox} size="lg" color={setColor(status)} />
-        <div className="container_row_text">
-          <p>
-            {props.name}
-            <span>{props.image}</span>
-          </p>
+      <div className="saved_container_row">
+        <div>
+          <FontAwesomeIcon icon={faBox} size="lg" color={setColor(status)} />
+        </div>
+        <div>{props.name}</div>
+        <div>{props.image}</div>
+        <div style={{ width: "15%" }}>
+          {(props.createdAt && <ReactTimeAgo date={new Date(props.createdAt)} locale="en-US" timeStyle={dateStyle} />) || "-"}
+        </div>
+        <div style={{ width: "15%" }}>
+          {(props.updateAt && <ReactTimeAgo date={new Date(props.updateAt)} locale="en-US" timeStyle={dateStyle} />) || "-"}
         </div>
       </div>
     </>
