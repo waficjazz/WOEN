64b10f505460c2ac7e43bab8f9e6b85c854e20d5
waficjazz <waficojazzaro@gmail.com>
2023-04-06 18:35:26 +0300

    parser structure

--- cli/run.py
+++ cli/run.py
@@ -38,12 +38,52 @@ def list_workflows():
     print(table)
 
 
+def list_templates():
+    url = SERVER + '/api/v1/workflow/all/templates/' + PROJECT_ID
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    except requests.exceptions.HTTPError as error:
+        print(f'Error: {error}')
+        return None
+
+    json_data = response.json()
+
+    table = PrettyTable(['NAME'])
+    table.border = False
+    table.align = 'l'
+    for obj in json_data:
+        row = [obj['name']]
+        table.add_row(row)
+
+    print(table)
+
+
+
 def main(args):
-    list_workflows()
+    if hasattr(args, 'list_args') and args.list_args == 'list':
+        list_workflows()
+
+    if hasattr(args, 'templates_args') and args.templates_args == 'list':
+        list_templates()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('filepath',nargs="?", help='Filepath of the YAML file to process' )
+    parser = argparse.ArgumentParser(prog='PROG' , description='Description of your program')
+    # parser.add_argument('list',help='Filepath of the YAML file to process' )
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_0 = subparsers.add_parser('list', help='a help'  )
+    parser_0.add_argument('list_args', help='a help' , action='store_const' , const='list')
+
+    parser_a = subparsers.add_parser('template', help='a help')
+    parser_a.add_argument('templates_args', help='a help' , choices=['list'])
+
+    
     args = parser.parse_args()
 
     main(args)
